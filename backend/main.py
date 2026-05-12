11252503436d1b9436182e5dd6205c076f870c6d
micaiahsamuel31 <micaiahsamuel31@gmail.com>
2026-05-12 12:46:21 +0530

    added deployment keys

--- backend/main.py
+++ backend/main.py
@@ -163,6 +163,7 @@ app = FastAPI(title="Todo API", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://to-do-app-frontend-8tuw.onrender.com"
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
