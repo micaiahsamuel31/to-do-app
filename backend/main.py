49069cc40c59c5be2f64f31e77a81a24b012c2d0
micaiahsamuel31 <micaiahsamuel31@gmail.com>
2026-05-12 14:30:12 +0530

    cors update

--- backend/main.py
+++ backend/main.py
@@ -164,8 +164,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://to-do-app-frontend-8tuw.onrender.com"
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        
+        
     ],
     #allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
     allow_credentials=True,
